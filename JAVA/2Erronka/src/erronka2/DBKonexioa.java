0c1fa92c0ac6e2fe43d2c2dcf8bf0c5cc0852128
ane-sot-azp <ane_sot_azp@goierrieskola.org>
2025-01-28 12:12:37 +0100

    DatuBasearekin konexioa sortu da

--- JAVA/2Erronka/src/erronka2/DBKonexioa.java
+++ JAVA/2Erronka/src/erronka2/DBKonexioa.java
@@ -1,5 +1,39 @@
 package erronka2;
 
-public class DBKonexioa {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+import java.util.Scanner;
+import java.sql.Statement;
 
-}
+public class DBKonexioa {
+	private static String ERABILTZAILEA = "administratzailea";
+	private static String PASAHITZA = "1erronka";
+	private static String DB = "2Erronka";
+	private static String URL = "jdbc:mysql://172.16.242.107:3306/"+DB;
+	
+	private static Connection conn;
+	private static Statement st_= null;
+
+	public void konexioaEgin() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			conn = DriverManager.getConnection(URL, ERABILTZAILEA, PASAHITZA);
+			
+			if (conn != null) {
+				st_ = conn.createStatement();
+				System.out.println("Konexioa ondo egin da");
+			}
+			else {
+				System.out.println("Konexioa gaizki atera da.");
+			}
+		} catch (SQLException e) {e.printStackTrace();}
+		catch (ClassNotFoundException e) {e.printStackTrace();}
+		catch (Exception e) {e.printStackTrace();}
+		}
+	}
