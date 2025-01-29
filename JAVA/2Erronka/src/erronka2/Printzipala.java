745387367b2ff8a47e70292a2dee77898cf668e4
zzMart1Nzz <b.martin.ulanga@gmail.com>
2025-01-29 12:40:15 +0100

    Administrazailearen menua

--- JAVA/2Erronka/src/erronka2/Printzipala.java
+++ JAVA/2Erronka/src/erronka2/Printzipala.java
@@ -45,9 +45,79 @@ public class Printzipala extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel lblEskHisto_2 = new JLabel("Historikoa");
+		lblEskHisto_2.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblEskHisto_2.setBounds(800, 221, 81, 18);
+		contentPane.add(lblEskHisto_2);
+		
+		JLabel lblEskaHisto_1 = new JLabel("Eskaeren");
+		lblEskaHisto_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblEskaHisto_1.setBounds(804, 204, 74, 18);
+		contentPane.add(lblEskaHisto_1);
+		
+		JLabel lblEsakerak = new JLabel("Eskaerak");
+		lblEsakerak.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblEsakerak.setBounds(804, 23, 71, 18);
+		contentPane.add(lblEsakerak);
+		
+		JLabel lblEskaera = new JLabel("Eskaera");
+		lblEskaera.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblEskaera.setBounds(627, 221, 62, 23);
+		contentPane.add(lblEskaera);
+		
+		JLabel lblPiezen = new JLabel("Piezen");
+		lblPiezen.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPiezen.setBounds(631, 204, 53, 18);
+		contentPane.add(lblPiezen);
+		
+		JLabel lblPiezak = new JLabel("Piezak");
+		lblPiezak.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblPiezak.setBounds(639, 23, 50, 18);
+		contentPane.add(lblPiezak);
+		
+		JLabel lblEskaeraIco_1 = new JLabel("New label");
+		lblEskaeraIco_1.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonEskaeraHistorikoa(1).png"));
+		lblEskaeraIco_1.setBounds(771, 251, 134, 107);
+		contentPane.add(lblEskaeraIco_1);
+		
+		JLabel lblPiezaEskIco = new JLabel("New label");
+		lblPiezaEskIco.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonPiezaEskaera(1).png"));
+		lblPiezaEskIco.setBounds(593, 251, 134, 107);
+		contentPane.add(lblPiezaEskIco);
+		
+		JLabel lblEskaeraIco = new JLabel("New label");
+		lblEskaeraIco.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonEskaera(1).png"));
+		lblEskaeraIco.setBounds(771, 52, 134, 107);
+		contentPane.add(lblEskaeraIco);
+		
+		JLabel lblPiezakIco = new JLabel("New label");
+		lblPiezakIco.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonPieza(1).png"));
+		lblPiezakIco.setBounds(593, 52, 134, 107);
+		contentPane.add(lblPiezakIco);
+		
+		JLabel lblBalorazioak = new JLabel("Balorazioak");
+		lblBalorazioak.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblBalorazioak.setBounds(444, 221, 91, 23);
+		contentPane.add(lblBalorazioak);
+		
+		JLabel lblLangileakIco_1_1 = new JLabel("New label");
+		lblLangileakIco_1_1.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonBalorazioa(1).png"));
+		lblLangileakIco_1_1.setBounds(413, 251, 134, 107);
+		contentPane.add(lblLangileakIco_1_1);
+		
+		JLabel lblBezeroak = new JLabel("Bezeroak");
+		lblBezeroak.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblBezeroak.setBounds(265, 221, 74, 23);
+		contentPane.add(lblBezeroak);
+		
+		JLabel lblLangileakIco_1 = new JLabel("New label");
+		lblLangileakIco_1.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonBezeroa(1).png"));
+		lblLangileakIco_1.setBounds(239, 251, 134, 107);
+		contentPane.add(lblLangileakIco_1);
+		
 		JLabel lblProdPrsTxt_1_1 = new JLabel("Prestakuntzan");
 		lblProdPrsTxt_1_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblProdPrsTxt_1_1.setBounds(61, 221, 115, 18);
+		lblProdPrsTxt_1_1.setBounds(70, 221, 115, 18);
 		contentPane.add(lblProdPrsTxt_1_1);
 		
 		JLabel lblProdPrsTxt_1 = new JLabel("Produktuak");
@@ -56,7 +126,7 @@ public class Printzipala extends JFrame {
 		contentPane.add(lblProdPrsTxt_1);
 		
 		JLabel lblProduktPresIco = new JLabel("New label");
-		lblProduktPresIco.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonProduktua(1).png"));
+		lblProduktPresIco.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\ikonProduktuaPrestakuntzan(1).png"));
 		lblProduktPresIco.setBounds(61, 251, 134, 107);
 		contentPane.add(lblProduktPresIco);
 		
@@ -65,34 +135,19 @@ public class Printzipala extends JFrame {
 		lblHornitzailtxt.setBounds(433, 23, 102, 18);
 		contentPane.add(lblHornitzailtxt);
 		
-		JButton btnLangileakIkusi_1 = new JButton("Sartu");
-		btnLangileakIkusi_1.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnLangileakIkusi_1.setBounds(433, 170, 91, 23);
-		contentPane.add(btnLangileakIkusi_1);
-		
 		JLabel lbLangileaktxt = new JLabel("Langileak");
 		lbLangileaktxt.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lbLangileaktxt.setBounds(265, 22, 74, 23);
+		lbLangileaktxt.setBounds(265, 23, 74, 23);
 		contentPane.add(lbLangileaktxt);
-		
-		JButton btnLangileakIkusi = new JButton("Sartu");
-		btnLangileakIkusi.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnLangileakIkusi.setBounds(265, 170, 91, 23);
-		contentPane.add(btnLangileakIkusi);
 		
 		JButton btnNewButton = new JButton("<--");
 		btnNewButton.setIcon(null);
 		btnNewButton.setBounds(0, 0, 59, 23);
 		contentPane.add(btnNewButton);
 		
-		JButton btnProdukIkusi = new JButton("Sartu");
-		btnProdukIkusi.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		btnProdukIkusi.setBounds(80, 170, 91, 23);
-		contentPane.add(btnProdukIkusi);
-		
 		JLabel lblProdtxt = new JLabel("Produktuak");
 		lblProdtxt.setFont(new Font("Tahoma", Font.PLAIN, 18));
-		lblProdtxt.setBounds(80, 24, 89, 18);
+		lblProdtxt.setBounds(80, 23, 89, 18);
 		contentPane.add(lblProdtxt);
 		
 		JLabel lblProduktuakIco = new JLabel("New label");
@@ -115,5 +170,9 @@ public class Printzipala extends JFrame {
 		lblFondoa.setIcon(new ImageIcon("C:\\Users\\benat\\Downloads\\UTech java fondoa 1(1).png"));
 		lblFondoa.setBounds(0, 0, 973, 434);
 		contentPane.add(lblFondoa);
+		
+		JLabel label = new JLabel("New label");
+		label.setBounds(814, 228, 48, 14);
+		contentPane.add(label);
 	}
 }
